abe94fcfb8eb7f706564a66428e05bee101c137b
Ryan Thompson <ryan@pyrocms.com>
2018-01-23 06:55:50 -0600

    Meh

--- resources/js/layout/layout.js
+++ resources/js/layout/layout.js
@@ -1,126 +1,38 @@
 (function (window, document) {
 
     const layout = document.querySelector('textarea[name="sections"]');
-    const start = layout.closest('.card');
+
+    const templates = {
+        'section': '<div class="card"></div>',
+        'pane': '<div class="tab-pane"></div>',
+        'wrapper': '<div class="card-block"></div>',
+        'fields': '<div class="field-group"></div>',
+        'tabs': `<ul class="nav nav-sections">
+                    <li class="nav-item active">
+                        <a data-toggle="tab" class="nav-link">
+                            Tab Name
+                        </a>
+                    </li>
+                </ul>`,
+        'tab': `<li class="nav-item active">
+            <a data-toggle="tab" class="nav-link">
+                Tab Name
+            </a>
+        </li>`,
+    };
 
     document.querySelector('a[data-add="tabs"]').addEventListener('click', function () {
 
-        start.insertAdjacentHTML('afterEnd', '<div class="card ">' +
+        let section = document.createRange().createContextualFragment(templates.section);
+        let wrapper = document.createRange().createContextualFragment(templates.wrapper);
 
-            '<ul class="nav nav-sections ">' +
-            '<li class="nav-item  active">' +
-            '<a href="#tab-general" data-toggle="tab" class="nav-link">' +
-            'General' +
-            '</a>' +
-            '</li>' +
-            '</ul>' +
+        wrapper.innerHTML = 'Ryan is awesome';
 
-            '<div class="card-block">' +
+        section.firstChild.appendChild(wrapper);
 
-            '<div class="tab-pane  active" id="tab-general">' +
+        console.log(wrapper);
 
-            '<div class="field-group">' +
-            '<div lang="en" class="form-group">' +
-
-            '<label class="control-label">' +
-            'Title' +
-
-            '<span class="required">*</span>' +
-
-            '</label>' +
-
-            '<p class="text-muted">Specify a short descriptive name for this page.</p>' +
-
-            '<div>' +
-            'INPUT' +
-            '</div>' +
-
-            '</div>' +
-
-            '</div>' +
-
-            '<div class="field-group">' +
-            '<div class="form-group">' +
-
-            '<label class="control-label">' +
-            'Slug' +
-
-            '<span class="required">*</span>' +
-
-            '</label>' +
-
-            '<p class="text-muted">The slug is used in building the page\'s URL.</p>' +
-
-
-            '<div>' +
-            'INPUT' +
-            '</div>' +
-
-            '</div>' +
-
-            '</div>' +
-
-            '</div>' +
-
-            '</div>' +
-
-            '</div>');
-
-        return false;
-    });
-
-    document.querySelector('a[data-add="section"]').addEventListener('click', function () {
-
-        start.insertAdjacentHTML('afterEnd', '<div class="card ">' +
-
-            '<div class="card-block">' +
-
-            '<div class="field-group">' +
-            '<div lang="en" class="form-group">' +
-
-            '<label class="control-label">' +
-            'Title' +
-
-            '<span class="required">*</span>' +
-
-            '</label>' +
-
-            '<p class="text-muted">Specify a short descriptive name for this page.</p>' +
-
-            '<div>' +
-            'INPUT' +
-            '</div>' +
-
-            '</div>' +
-
-            '</div>' +
-
-            '<div class="field-group">' +
-            '<div class="form-group">' +
-
-            '<label class="control-label">' +
-            'Slug' +
-
-            '<span class="required">*</span>' +
-
-            '</label>' +
-
-            '<p class="text-muted">The slug is used in building the page\'s URL.</p>' +
-
-
-            '<div>' +
-            'INPUT' +
-            '</div>' +
-
-            '</div>' +
-
-            '</div>' +
-
-            '</div>' +
-
-            '</div>');
-
-        return false;
+        document.querySelector('form .card:last-child').parentNode.insertBefore(section, document.querySelector('form .card:last-child').nextSibling);
     });
 
 })(window, document);
